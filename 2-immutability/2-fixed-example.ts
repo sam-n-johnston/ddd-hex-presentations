108b9e7d5ae7640c5db6c0cb6e2dc2467d777cbc
Sam-Nicolai Johnston <sam-nicolai.johnston@ssense.com>
2022-04-15 16:17:58 -0400

    updte VO immutability example

--- 2-immutability/2-fixed-example.ts
+++ 2-immutability/2-fixed-example.ts
@@ -1,34 +1,24 @@
 class Item {
-    private constructor (
+    private constructor(
         private readonly _sku: string,
         private readonly _quantity: number
     ) {}
 
-    public static create(sku: string, quantity: number){
-        return new Item(sku, quantity)
+    public static create(sku: string, quantity: number) {
+        return new Item(sku, quantity);
     }
 }
 
 class OrderService {
     // ...
 
-    removeItem (order: Order, item:  Item) {
-        if (item.quantity < 0) throw new Error('invalid!')
+    removeItem(order: Order, item: Item) {
+        if (item.quantity < 0) throw new Error('invalid!');
 
-        order.removeItem(item)
-        
+        order.removeItem(item);
+
         // Item CANNOT be changed
-        this.stockService.removeItem(order.id, item)
-        this.warehouseService.removeItem(order.id, item)
-    }
-}
-
-class Order {
-    // ...
-
-    removeItem (item: Item) {
-        if (item.quantity <= 0) throw new Error('Item is not valid')
-
-        item.quantity = 0
+        this.stockService.removeItem(order.id, item);
+        this.warehouseService.removeItem(order.id, item);
     }
 }
